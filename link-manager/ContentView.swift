0c7d97ee421327abb9ffb3a599f921749d5354eb
Bahar Tehranipoor <tehranipoorbp@gmail.com>
2020-08-16 12:31:02 -0400

    Initial Commit

---
+++ link-manager/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  link-manager
+//
+//  Created by Productivity on 8/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
